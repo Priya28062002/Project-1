80febe65a8ea5470fd3bcc6299046cfeca419588
Priya28062002 <priyaanand0628@gmail.com>
2023-03-31 14:40:03 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+         <h1>let see the output</h1>
+         <button onClick={window['alertmsg']}>click to see</button>
+         
+      </header>
+    </div>
+  );
+}
+
+export default App;
